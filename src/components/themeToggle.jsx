1803bd54cadb7b987bc5a1924de4200d31c976f2
shubhpatr <shubham.mohapatra1996@gmail.com>
2021-05-19 17:03:01 +0530

    Added Evolution Info in Modal

--- src/components/themeToggle.jsx
+++ src/components/themeToggle.jsx
@@ -9,12 +9,12 @@ const Toggle = () => {
 		<div className="transition duration-500 ease-in-out rounded-full p-2">
 			{theme === "dark" ? (
 				<HiSun
-					onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+					onClick={() => setTheme("light")}
 					className="text-gray-500 dark:text-white text-2xl cursor-pointer"
 				/>
 			) : (
 				<HiMoon
-					onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+					onClick={() => setTheme("dark")}
 					className="text-black dark:text-gray-400 text-2xl cursor-pointer"
 				/>
 			)}
